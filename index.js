927c2cac868d07cd5281d0129d813ef52f30451d
藤崎優樹 <yu-ki@yukimbp15.local>
2018-12-22 06:39:11 +0900

    no change

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 5000;
 const line = require("@line/bot-sdk");
-const config = {
+const configStellar = {
   channelAccessToken: process.env.ACCESS_TOKEN,
   channelSecret: process.env.SECRET_KEY
 };
-const client = new line.Client(config); // 追加
+const configDX = {
+  channelAccessToken: process.env.DX_ACCESS,
+  channelSecret: process.env.DX_SECRET
+}
+const Knights = new line.Client(configStellar); // 追加
+const DX = new line.Client(configDX);
 
 express()
   .use(express.static(path.join(__dirname, "public")))
@@ -15,10 +20,12 @@ express()
   .get("/", (req, res) => res.render("pages/index"))
   .get("/g/", (req, res) => res.json({ method: "こんにちは、getさん" }))
   .post("/p/", (req, res) => res.json({ method: "こんにちは、postさん" }))
-  .post("/hook/", line.middleware(config), (req, res) => lineBot(req, res))
+  .post("/SK/", line.middleware(Knights), (req, res) => StellarKnights(req, res))
+  .post("/DX/",line.middleware(DX),(req,res) => DoubleCross(req,res))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-function lineBot(req, res) {
+
+function StellarKnights(req, res) {
   res.status(200).end();
   // ここから追加
   const events = req.body.events;
@@ -26,21 +33,21 @@ function lineBot(req, res) {
   console.log("event fire");
   for (let i = 0, l = events.length; i < l; i++) {
     const ev = events[i];
-    echoman(ev)
+    Stellar(ev)
   }
   console.log("all event complete");
 }
 var buke_count = 0;
 // 追加
-async function echoman(ev) {
-  const pro =  await client.getProfile(ev.source.userId);
+async function Stellar(ev) {
+  const pro =  await Knights.getProfile(ev.source.userId);
 
   var command = ev.message.text;
 
   if(command.match('開始') || command.match('スタート'))
   {
     buke_count = 0;
-    return client.replyMessage(ev.replyToken, {
+    return Knights.replyMessage(ev.replyToken, {
       type: "text",
       text: `ブーケのカウントを開始します。`
     })
@@ -53,7 +60,7 @@ async function echoman(ev) {
   else if(command.match('ストップ') || command.match('終了'))
   {
 
-    return client.replyMessage(ev.replyToken, {
+    return Knights.replyMessage(ev.replyToken, {
       type: "text",
       text: `ブーケのカウントを終了します。\nブーケの個数は`+buke_count+'でした。'
     })
@@ -78,7 +85,7 @@ async function echoman(ev) {
       resultA += tmp.toString();
       sum += tmp;
     }
-    return client.replyMessage(ev.replyToken, {
+    return Knights.replyMessage(ev.replyToken, {
       type: "text",
       text: 'result:'+sum+'('+resultA+')'
     })
@@ -124,7 +131,7 @@ async function echoman(ev) {
       count = ncount;
       ncount = 0;
     }
-    return client.replyMessage(ev.replyToken, {
+    return Knights.replyMessage(ev.replyToken, {
       type: "text",
       text: 'result:'+sum
     })
@@ -132,5 +139,72 @@ async function echoman(ev) {
   else
   {
     console.log("no adaptive text : "+command);
+  }
+}
+
+function DoubleCross(req,res)
+{
+  es.status(200).end();
+  // ここから追加
+  const events = req.body.events;
+  const promises = [];
+  console.log("event fire");
+  for (let i = 0, l = events.length; i < l; i++) {
+    const ev = events[i];
+    Cross(ev)
+  }
+  console.log("all event complete");
+}
+
+async function Cross(ev)
+{
+  const pro =  await Knights.getProfile(ev.source.userId);
+
+  var command = ev.message.text;
+  if(command.match('[0-9]{1,2}D10 c[0-9]'))
+  {
+    var xx = command.match('[0-9]{1,2}D10 c[0-9]');
+    var index   = command.indexOf('D');
+    var count = command.substring(0,index);
+    var critical = command.slice(-1);
+    var dice = 10;
+    var isContinue = true;
+    var ncount = 0;
+    var max = 0;
+    var sum = 0;
+    console.log('command : '+command+' count : '+count + ' c : '+critical);
+    while(isContinue)
+    {
+      max = -1;
+      for(var i = 0;i<count;i++)
+      {
+        var tmp = Math.floor(Math.random() * dice);
+        if(max < tmp)
+        {
+          max = tmp;
+        }
+        if(tmp > critical)
+        {
+          ncount++;
+        }
+      }
+      if(ncount > 0)
+      {
+        sum += 10;
+        console.log("next stage");
+      }
+      else
+      {
+        sum += max;
+        console.log("end stage");
+        isContinue = false;
+      }
+      count = ncount;
+      ncount = 0;
+    }
+    return DX.replyMessage(ev.replyToken, {
+      type: "text",
+      text: 'result:'+sum
+    })
   }
 }
