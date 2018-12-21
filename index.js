26c78aac539591468ce7464df66322fc668cdef7
藤崎優樹 <yu-ki@yukimbp15.local>
2018-12-21 16:16:27 +0900

    add line return

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
+const line = require("@line/bot-sdk"); // 追加
+// 追加
+const config = {
+  channelAccessToken: process.env.ACCESS_TOKEN,
+  channelSecret: process.env.SECRET_KEY
+};
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
@@ -9,5 +14,29 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/g/',(req,res) => res.json({method: "こんにちは、getさん"}))
   .post('/p/',(req,res) => res.json({method:"こんにちは、postさん"}))
-  .post('/hook/',(req,res) => res.json({test:"test"}))
+  .post('/hook/',line.middleware(config),(req,res) => lineBot(req,res))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+function lineBot(req,res)
+{
+  res.status(200).end(); 
+  console.log("pass"); // 追加
+  const events = req.body.events;
+  const promises = [];
+  for (let i = 0, l = events.length; i < l; i++) {
+    const ev = events[i];
+    promises.push(
+      echoman(ev)
+    );
+  }
+  Promise.all(promises).then(console.log("pass"));
+}
+
+async function echoman(ev) {
+  const pro =  await client.getProfile(ev.source.userId);
+  return client.replyMessage(ev.replyToken, {
+    type: "text",
+    text: `${pro.displayName}さん、今「${ev.message.text}」って言いました？`
+  })
+}
