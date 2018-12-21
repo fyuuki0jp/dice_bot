d034fe1ca63d680dd695cb11b6d84004094eda7f
藤崎優樹 <yu-ki@yukimbp15.local>
2018-12-21 20:57:32 +0900

    test

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-const line = require("@line/bot-sdk"); // 追加
-// 追加
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+const line = require("@line/bot-sdk");
 const config = {
   channelAccessToken: process.env.ACCESS_TOKEN,
   channelSecret: process.env.SECRET_KEY
 };
 const client = new line.Client(config); // 追加
 
-
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/g/',(req,res) => res.json({method: "こんにちは、getさん"}))
-  .post('/p/',(req,res) => res.json({method:"こんにちは、postさん"}))
-  .post('/hook/',line.middleware(config),(req,res) => lineBot(req,res))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .use(express.static(path.join(__dirname, "public")))
+  .set("views", path.join(__dirname, "views"))
+  .set("view engine", "ejs")
+  .get("/", (req, res) => res.render("pages/index"))
+  .get("/g/", (req, res) => res.json({ method: "こんにちは、getさん" }))
+  .post("/p/", (req, res) => res.json({ method: "こんにちは、postさん" }))
+  .post("/hook/", line.middleware(config), (req, res) => lineBot(req, res))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-
-function lineBot(req,res)
-{
-  res.status(200).end(); 
+function lineBot(req, res) {
+  res.status(200).end();
+  // ここから追加
   const events = req.body.events;
   const promises = [];
   for (let i = 0, l = events.length; i < l; i++) {
@@ -35,6 +32,7 @@ function lineBot(req,res)
   Promise.all(promises).then(console.log("pass"));
 }
 
+// 追加
 async function echoman(ev) {
   const pro =  await client.getProfile(ev.source.userId);
   return client.replyMessage(ev.replyToken, {
