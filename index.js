25ead0601bf9052d8706124874ba27e9785ea73c
藤崎優樹 <yu-ki@yukimbp15.local>
2018-12-22 15:15:06 +0900

    bugfix

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const config = {
 const client = new line.Client(config); // 追加
 
 server.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("listen socket.io server");
 });
 
 express()
