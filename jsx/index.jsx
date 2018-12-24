ddeb560fd7c35b84f34048a50be2516ef330ee48
藤崎優樹 <yu-ki@yukimbp15.local>
2018-12-24 14:11:55 +0900

    bugfixed

--- jsx/index.jsx
+++ jsx/index.jsx
@@ -1,131 +1,21 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var DropZone = require('react-dropzone');
-const { getMuiTheme, MuiThemeProvider } = require('material-ui/styles');
-var { AppBar, Drawer, MenuItem } = require('material-ui');
 var socket = io.connect();
-
-class Home extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <h1>Hello World!</h1>
-        )
-    }
-}
-
-class Keaper extends React.Component {
-    constructor(props) {
-        super(props);
-        this.sendfile = this.sendfile.bind(this);
-    }
-    sendfile(file) {
-        var reader = new FileReader();
-        var send_file = file;
-        reader.onload = (event) => {
-            data.file = event.target.result;
-            data.name = send_file;
-            data.type = "image/jpg";
-
-            socket.emit('image', data);
-        };
-
-        reader.readAsBinaryString(send_file);
-    }
-    render() {
-        var left = {
-            width: "70%",
-            height:"100%"
-        };
-        var right = {
-            left: "75%",
-            width: "25%",
-            background: "#f2f2f3"
-        };
-        return (
-            <MuiThemeProvider muiTheme={getMuiTheme()}>
-                <div draggable={true} onDrop={this.sendfile} style={left}>
-                <div>画像をドラックまたはクリック</div>
-                <img id="view"></img>
-                </div>
-                <div draggable={false} style={right}>
-
-                </div>
-                </MuiThemeProvider>
-        )
-    }
-}
-
-class Version extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <h1>バージョン情報</h1>
-
-        )
-    }
-}
 
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            open: false,
-            mode: 0
-        };
-        this.OpenMenu = this.OpenMenu.bind(this);
-        this.HideMenu = this.HideMenu.bind(this);
-        this.Home = this.Home.bind(this);
-        this.Keaper = this.Keaper.bind(this);
-        this.Version = this.Version.bind(this);
 
-    }
-    OpenMenu() {
-        this.setState({ open: true });
-    }
-    HideMenu() {
-        this.setState({ open: false });
-    }
-    Home() {
-        this.setState({ open: false, mode: 0 });
-    }
-    Keaper() {
-        this.setState({ open: false, mode: 1 });
-    }
-    Version() {
-        this.setState({ open: false, mode: 2 });
     }
     render() {
-
-        var main = <Home />
-        if (this.state.mode == 1) {
-            main = <Keaper />
+        var titileStyle = {
+            width:"100%",
+            background:"#000F0F"
         }
-
         return (
-            <MuiThemeProvider muiTheme={getMuiTheme()}>
-                <div id="page">
-                    <div id="menu">
-                        <AppBar
-                            title={"TRPGシステム(大工大TRPGサークル)"}
-                            iconClassNameRight="muidocs-icon-navigation-expand-more"
-                            onLeftIconButtonClick={this.OpenMenu}
-                        />
-                        <Drawer open={this.state.open} width="30%" docked={true}>
-                            <MenuItem onClick={this.Home}>ホーム</MenuItem>
-                            <MenuItem onClick={this.Keaper}>GM管理画面</MenuItem>
-                        </Drawer>
-                    </div>
-                    <div id="main" onClick={this.HideMenu}>
-                        {main}
-                    </div>
-                </div>
-
-            </MuiThemeProvider>
+            <div id="title" style={titileStyle}>
+                <h1>TRPG　GM管理画面</h1>
+            </div>
         )
     }
 }
